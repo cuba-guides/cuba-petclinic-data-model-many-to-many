cfdf6ea21e91fe39917e9658bdf745808d85d2e3
Mario <mario@die-davids.de>
2019-01-05 17:15:43 +0100

    added direct many to many association to Vet <--> Specialty

--- modules/global/src/com/haulmont/sample/petclinic/entity/vet/Specialty.java
+++ modules/global/src/com/haulmont/sample/petclinic/entity/vet/Specialty.java
@@ -1,12 +1,26 @@
 package com.haulmont.sample.petclinic.entity.vet;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
 import com.haulmont.sample.petclinic.entity.NamedEntity;
+
+import javax.persistence.*;
+import java.util.List;
 
 @Table(name = "PETCLINIC_SPECIALTY")
 @Entity(name = "petclinic_Specialty")
 public class Specialty extends NamedEntity {
     private static final long serialVersionUID = -1397207634482158673L;
 
+    @JoinTable(name = "PETCLINIC_VET_SPECIALTY_LINK",
+            joinColumns = @JoinColumn(name = "SPECIALTY_ID"),
+            inverseJoinColumns = @JoinColumn(name = "VET_ID"))
+    @ManyToMany
+    protected List<Vet> vets;
+
+    public List<Vet> getVets() {
+        return vets;
+    }
+
+    public void setVets(List<Vet> vets) {
+        this.vets = vets;
+    }
 }
--- modules/web/src/com/haulmont/sample/petclinic/web/vet/specialty/SpecialtyEdit.java
+++ modules/web/src/com/haulmont/sample/petclinic/web/vet/specialty/SpecialtyEdit.java
@@ -3,9 +3,10 @@ package com.haulmont.sample.petclinic.web.vet.specialty;
 import com.haulmont.cuba.gui.screen.*;
 import com.haulmont.sample.petclinic.entity.vet.Specialty;
 
+
 @UiController("petclinic_Specialty.edit")
 @UiDescriptor("specialty-edit.xml")
-@EditedEntityContainer("specialtyCt")
+@EditedEntityContainer("specialtyDc")
 @LoadDataBeforeShow
 public class SpecialtyEdit extends StandardEditor<Specialty> {
 }
